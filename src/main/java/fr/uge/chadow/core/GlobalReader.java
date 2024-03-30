917dee71a494d353925db388288b73b8b6d02a05
valse <val.sebb@gmail.com>
2024-03-30 12:33:58 +0100

    first global reader class

---
+++ src/main/java/fr/uge/chadow/core/GlobalReader.java
@@ -0,0 +1,32 @@
+package fr.uge.chadow.core;
+
+import java.nio.ByteBuffer;
+
+public class GlobalReader<T extends Record> implements Reader<T> {
+  private enum State {
+    DONE, WAITING, ERROR
+  }
+
+  private State state = State.WAITING;
+
+  private T value;
+
+  @Override
+  public ProcessStatus process(ByteBuffer bb) {
+    if (state == State.DONE || state == State.ERROR) {
+      throw new IllegalStateException();
+    }
+    return null;
+  }
+
+  @Override
+  public T get() {
+
+    return null;
+  }
+
+  @Override
+  public void reset() {
+
+  }
+}
